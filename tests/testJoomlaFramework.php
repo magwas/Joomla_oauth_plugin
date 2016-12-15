fe9b7be1f6db92ba3cf3727331cd5e0fa6c6394f
utopszkij <tibor.fogler@gmail.com>
2016-12-15 16:05:13 +0200

    V4.01 logout támogatás, regist extra field támogatás

--- tests/testJoomlaFramework.php
+++ tests/testJoomlaFramework.php
@@ -28,7 +28,7 @@ class testDataClass {
 	protected $dbErrorMsg;
 	protected $dbIndex;
 
-    public $gotArgs = array();
+    	public $gotArgs = array();
 	public $mock_data = array();
 	/**
 	* set remoteCall results for test
@@ -124,7 +124,7 @@ class JFactory {
 		global $testUser;
 		if (!isset($testUser)) $testUser = new JUser();
 		$testUser->id = $id;
-		$testUser->username='testElek';
+		$testUser->username='guest';
 		return $testUser;
 	}
 	public static  function getLanguage() {
@@ -138,7 +138,7 @@ class JFactory {
 }
 class JApplication {
 	public $input;
-    	function __construct() {
+	    function __construct() {
 		$this->input = new JInput();
 	}
 	public function getUserStateFromRequest($name, $default='',$dataType='') {
@@ -148,10 +148,17 @@ class JApplication {
 		return $default;
 	}
 	public function login($credentials) {
+		global $testUser;
+		$testUser->id = 1;
+		$testUser->username='testElek';
+		$testUser->name='Test Elek';
 		return true;
 	}	
 	public function logout() {
-		return true;
+		global $testUser;
+		$testUser->id=0;
+		$testUser->username = 'guest';
+		$testUser->name = '';
 	}
 }
 class JDocument {
@@ -377,6 +384,7 @@ class JControllerLegacy {
 		global $testData;
         $testData->mock_data["redirectURI"] = $this->redirectURI;
 		$testData->mock_data["redirectMsg"] = $message;
+		echo 'redirect:'.$this->redirectURI.' message='.$message."\n";
 	}
 	public function edit() {
 		echo 'joomla default edit task';
